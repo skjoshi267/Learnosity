b1fbe4612468cd24cbd8cdc562985da3bf673147
Shreyank Joshi <sk.joshi267@gmail.com>
2021-08-20 13:52:01 +0100

    Completed barplots in Streamlit

--- config/config.py
+++ config/config.py
@@ -3,21 +3,43 @@ import numpy as np
 
 class Configuration:
     DATA_PATH = "data/"
-    FILES_TO_LOAD = ["sessions-","sessionsquestions-","tags","activities","items"]
+    FILES_TO_LOAD = ["sessions-","sessionsquestions-","items_tags","activities","items"]
+    DATE_COLUMNS = ["session_dt_started","session_dt_completed","question_dt_created","question_dt_updated"]
 
 class Data:
     def __init__(self, raw_data):
         self.sessions = raw_data[0]
         self.session_questions = raw_data[1]
-        self.tags = raw_data[2]
+        self.items_tags = raw_data[2]
         self.activities = raw_data[3]
         self.items = raw_data[4]
 
-    def transform_tags(self):
-        tags = self.tags
-        tags = tags[tags.tagtype == "COURSE"]
-        indexes = tags[tags.tag == "-"].index
-        self.tags = tags.drop(indexes, axis = 0).reset_index(drop = True)
+    def transform_sessions(self):
+        #Transform Sessions Data
+        temp_sessions = self.sessions.drop(["activity_id","session_dt_saved","user_dt_created","activity_template_id","item_pool_id","current_time"], axis=1)
+        for dt_type in Configuration.DATE_COLUMNS:
+            if dt_type in temp_sessions.columns.tolist():
+                temp_sessions[dt_type] = pd.to_datetime(temp_sessions[dt_type])
+            else:
+                pass
+        self.sessions = temp_sessions
+    
+    def transform_sessionsq(self):
+        #Transform Sessions Questions
+        temp_sessionsq = self.session_questions#.drop(["activity_id","session_dt_saved","user_dt_created","activity_template_id","item_pool_id","current_time"], axis=1)
+        for dt_type in Configuration.DATE_COLUMNS:
+            if dt_type in temp_sessionsq.columns.tolist():
+                temp_sessionsq[dt_type] = pd.to_datetime(temp_sessionsq[dt_type])
+            else:
+                pass
+        self.sessionsq = temp_sessionsq
+
+    def join_selected(self,tagtype):
+        #Join Data of Sessions, SessionsQuestions and Item Tags
+        filter_data = self.items_tags[self.items_tags.tagtype == tagtype]
+        join_data = self.session_questions.merge(filter_data, on="item_reference")
+        self.sessions_db = self.sessions.merge(join_data, on=["session_id","user_id"])
+        return self.sessions_db
 
 class UI:
     sidebar_select = None
--- load_data.py
+++ load_data.py
@@ -1,4 +1,5 @@
 from attr import NOTHING
+from matplotlib import pyplot as plt
 from streamlit.elements.arrow import Data
 from config.config import Configuration as cfg, Data as data, UI as ui
 import pandas as pd
@@ -6,7 +7,7 @@ import numpy as np
 import glob
 import streamlit as st
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def load_data():
     file_paths = [(cfg.DATA_PATH + each_file + '*.csv') for each_file in cfg.FILES_TO_LOAD]
     files_list = [glob.glob(path) for path in file_paths]
@@ -24,21 +25,48 @@ def assign_data(file_data):
     return data_obj
 
 def transform_data(data_obj):
-    #Transform Tags
-    data_obj.transform_tags()
+    #Transform Data
+    data_obj.transform_sessions()
+    data_obj.transform_sessionsq()
 
 def build_ui_data(data_obj):
     if ui.sidebar_select == None:
-        sidebar_select_data = data_obj.tags.tag.unique().tolist()
+        sidebar_select_data = data_obj.items_tags.tagtype.unique().tolist()
         ui.sidebar_select = sidebar_select_data
 
 def run_app(data_obj):
+    #Title
     st.title("Learnosity - User Dashboard")
     st.sidebar.title("Criterion")
-    ui.selected = st.sidebar.selectbox("Select a Tag for Course", ui.sidebar_select)
     
-    st.header("Raw Data")
-    st.dataframe(data = data_obj.tags[data_obj.tags.tag == ui.selected])
+    #Select an option
+    ui.selected = st.sidebar.selectbox("Select a Tag Type", ui.sidebar_select)
+    st.header("Session Data")
+    st.subheader("by TagType")
+
+    #Build Data
+    sessions_data = data_obj.join_selected(ui.selected)
+    st.dataframe(data = sessions_data.head())
+    st.write("Total Count = ",len(sessions_data.index))
+
+    #Descriptive Statistics
+    st.header("Descriptive Statistics")
+    st.dataframe(data = sessions_data.describe())
+
+    #Bar Plot
+    st.header("Distribution of Scores for "+ui.selected+" tag")
+    st.subheader("Questions Scored and Maximum Scores per tagtype")
+    st.success("This graph describes scored obtained by a user per tagtype for a tag and the maximum score for questions per tagtype. It entails a\
+        fundamental analysis of the chapters that are easy to score and chapters that are difficult to score across the number of users.")
+    sessions_data_score = sessions_data[["tag","question_score","question_max_score"]].groupby(["tag"]).sum()
+    st.bar_chart(sessions_data_score)
+
+    st.subheader("Questions Attempted per Total Questions for each tagtype")
+    st.success("This graph describes the total number of questions attempted vs question not attempted for a particular tagtype. It demonstrates \
+        the comparison between number of questions per tagtype and an approximate level of difficlty for not attempting questions")
+    sessions_data_qa = sessions_data.pivot_table(index="tag",columns="question_attempted",aggfunc="size",fill_value=0)
+    sessions_data_qa_df = pd.DataFrame(sessions_data_qa.to_records()).set_index("tag").rename(columns={"0":"Not_Attempted","1":"Attempted"})
+    st.line_chart(sessions_data_qa_df)
 
 
 
